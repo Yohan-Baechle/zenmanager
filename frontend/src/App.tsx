15107963456118aec217c172fd1f9785174f9be0
Oriloo <112803541+Oriloo@users.noreply.github.com>
2025-10-07 09:54:36 +0200

    feat: initialize React + TypeScript + Vite project with Tailwind CSS and ESLint configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+      <div className="h-screen w-screen flex flex-col justify-center items-center gap-4 bg-black text-white">
+          <h1 className="text-4xl font-bold">Salut !</h1>
+          <p className="text-lg">
+              This is a React + TypeScript + Tailwind CSS project.
+          </p>
+      </div>
+  )
+}
+
+export default App
