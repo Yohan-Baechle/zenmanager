60c2ed78c274efe7c9acb1d76c45552006227190
Yohan-Baechle <baechle.yohan@gmail.com>
2025-10-08 13:23:02 +0200

    Swagger UI for Users OK

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
