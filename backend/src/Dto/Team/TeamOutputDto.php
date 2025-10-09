172aad66d1b122b6772c8a232abe36582a1e0231
Yohan-Baechle <baechle.yohan@gmail.com>
2025-10-09 10:03:18 +0200

    TeamInputDto created

--- backend/src/Dto/Team/TeamOutputDto.php
+++ backend/src/Dto/Team/TeamOutputDto.php
@@ -2,10 +2,16 @@
 
 namespace App\Dto\Team;
 
+use App\Dto\User\UserOutputDto;
+
 class TeamOutputDto
 {
     public function __construct(
         public readonly int $id,
         public readonly string $name,
+        public readonly ?string $description,
+        public readonly ?UserOutputDto $manager,
+        public readonly \DateTimeImmutable $createdAt,
+        public readonly \DateTimeImmutable $updatedAt,
     ) {}
 }
