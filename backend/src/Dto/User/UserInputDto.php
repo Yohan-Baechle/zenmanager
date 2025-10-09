c01210542886c460a91b28e0584f763a6effc30d
Yohan-Baechle <baechle.yohan@gmail.com>
2025-10-09 09:45:51 +0200

    UserInputDto created

---
+++ backend/src/Dto/User/UserInputDto.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Dto\User;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class UserInputDto
+{
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    public ?string $email = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 8)]
+    public ?string $password = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 100)]
+    public ?string $firstName = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 100)]
+    public ?string $lastName = null;
+
+    #[Assert\Regex(
+        pattern: '/^\+?[1-9]\d{1,14}$/',
+        message: 'Invalid phone number format'
+    )]
+    public ?string $phoneNumber = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Choice(choices: ['employee', 'manager'], message: 'Role must be either employee or manager')]
+    public ?string $role = null;
+
+    public ?int $teamId = null;
+}
