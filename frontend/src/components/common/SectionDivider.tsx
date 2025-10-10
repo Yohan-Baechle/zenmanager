8bee285553284cc746b2aabdec394e3235bbf5ce
Oriloo <112803541+Oriloo@users.noreply.github.com>
2025-10-10 16:45:41 +0200

    fix(SectionDivider): update text color to use var(--c3) for improved visibility

--- frontend/src/components/common/SectionDivider.tsx
+++ frontend/src/components/common/SectionDivider.tsx
@@ -8,7 +8,7 @@ export default function SectionDivider({ label, space }: SectionDividerProps) {
         <div className={`relative mx-2 ${space ? `my-${space}` : 'my-4'}`}>
             <div className="border-t border-[var(--c2)]"></div>
             {label &&
-                <p className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] px-2 text-xs font-semibold text-[var(--c2)] bg-[var(--c1)] uppercase tracking-wider">
+                <p className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] px-2 text-xs font-semibold text-[var(--c3)] bg-[var(--c1)] uppercase tracking-wider">
                     {label}
                 </p>
             }
