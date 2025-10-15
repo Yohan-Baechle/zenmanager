d54015ae0bc8ff85421e56a58377119fabbb86ea
Oriloo <pol-mattis.harquet@epitech.eu>
2025-10-15 16:24:53 +0200

    feat(clock): add icons to clock history table headers and update status display

--- frontend/src/components/common/Table.tsx
+++ frontend/src/components/common/Table.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react'
 
 interface Column<T> {
     header: string
+    icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>
     accessor: keyof T | ((row: T) => ReactNode)
     className?: string
 }
@@ -14,22 +15,26 @@ interface TableProps<T> {
 export default function Table<T>({ data, columns }: TableProps<T>) {
     return (
         <div className="overflow-x-auto">
-            <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
+            <table className="min-w-full divide-y divide-[var(--c2)]">
+                <thead className="">
                 <tr>
-                    {columns.map((column, index) => (
-                        <th
-                            key={index}
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                            {column.header}
-                        </th>
-                    ))}
+                    {columns.map((column, index) => {
+                        const Icon = column.icon
+                        return (
+                            <th
+                                key={index}
+                                className="px-3 py-3 text-left text-[var(--c5)] uppercase tracking-wider"
+                            >
+                                {Icon && <Icon className="inline h-5 w-5 mr-1" />}
+                                {column.header}
+                            </th>
+                        )
+                    })}
                 </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
+                <tbody className="divide-y divide-[var(--c2)]">
                 {data.map((row, rowIndex) => (
-                    <tr key={rowIndex} className="hover:bg-gray-50">
+                    <tr key={rowIndex} className="hover:bg-[var(--c2)]/25">
                         {columns.map((column, colIndex) => (
                             <td key={colIndex} className={`px-6 py-4 whitespace-nowrap ${column.className || ''}`}>
                                 {typeof column.accessor === 'function'
--- frontend/src/components/features/clocks/ClockHistory.tsx
+++ frontend/src/components/features/clocks/ClockHistory.tsx
@@ -2,6 +2,7 @@ import type { Clock } from '../../../types/clock.types'
 import Table from '../../common/Table'
 import Card from '../../common/Card'
 import { HistoryIcon } from "../../../assets/icons/history.tsx";
+import { AccountCircleIcon } from "../../../assets/icons/account-circle.tsx";
 
 interface ClockHistoryProps {
     clocks: Clock[]
@@ -11,23 +12,27 @@ export default function ClockHistory({ clocks }: ClockHistoryProps) {
     const columns = [
         {
             header: 'Nom',
+            icon: AccountCircleIcon,
             accessor: (clock: Clock) => `${clock.owner.firstName} ${clock.owner.lastName}`
         },
         {
             header: 'Date',
+            icon: HistoryIcon,
             accessor: (clock: Clock) => new Date(clock.time).toLocaleDateString()
         },
         {
             header: 'Heure',
+            icon: HistoryIcon,
             accessor: (clock: Clock) => new Date(clock.time).toLocaleTimeString()
         },
         {
             header: 'Type',
+            icon: HistoryIcon,
             accessor: (clock: Clock) => (
-                <span className={`px-2 py-1 rounded text-xs ${
-                    clock.status ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
+                <span className={`text-sm font-medium text-[var(--c1)] bg-[var(--c4)] px-2 py-0.5 rounded-full inline-block w-fit ${
+                    clock.status ? '' : ''
                 }`}>
-                  {clock.status ? 'Entrée' : 'Sortie'}
+                  {clock.status ? '↓ Entrée' : '↑ Sortie'}
                 </span>
             )
         },
