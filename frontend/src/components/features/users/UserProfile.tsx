09a7468bd5e2f9189f2b6b9b1a78e813710e784b
Oriloo <pol-mattis.harquet@epitech.eu>
2025-10-15 21:47:37 +0200

    feat(user-profile): add icons for user profile fields

--- frontend/src/components/features/users/UserProfile.tsx
+++ frontend/src/components/features/users/UserProfile.tsx
@@ -1,6 +1,13 @@
 import type {User} from '../../../types/user.types'
 import SectionDivider from "../../common/SectionDivider.tsx";
 import Button from "../../common/Button.tsx";
+import {AccountCircleIcon} from "../../../assets/icons/account-circle.tsx";
+import {SupervisorAccountIcon} from "../../../assets/icons/supervisor-account.tsx";
+import {CalendarTodayIcon} from "../../../assets/icons/calendar-today.tsx";
+import {AdminPanelSettingsIcon} from "../../../assets/icons/admin-panel-settings.tsx";
+import {AlternateEmailIcon} from "../../../assets/icons/alternate-email.tsx";
+import {IdCardIcon} from "../../../assets/icons/id-card.tsx";
+import {PhoneInTalkIcon} from "../../../assets/icons/phone-in-talk.tsx";
 
 interface UserProfileProps {
     data: User
@@ -10,28 +17,52 @@ export default function UserProfile({ data }: UserProfileProps) {
     return (
         <div className="">
             <div className="grid grid-cols-1 sm:grid-cols-[max-content_1fr] gap-0 items-center text-[var(--c5)]">
-                <label className="pl-5 pr-10 font-semibold">Prénom</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <AccountCircleIcon className="w-5 h-5"/>
+                    <label>Prénom</label>
+                </div>
                 <p className="">{data.firstName}</p>
                 <div className="sm:col-span-2"><SectionDivider/></div>
-                <label className="pl-5 pr-10 font-semibold">Nom</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <AccountCircleIcon className="w-5 h-5"/>
+                    <label>Nom</label>
+                </div>
                 <p className="">{data.lastName}</p>
                 <div className="sm:col-span-2"><SectionDivider/></div>
-                <label className="pl-5 pr-10 font-semibold">Identifiant</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <IdCardIcon className="w-5 h-5"/>
+                    <label>Identifiant</label>
+                </div>
                 <p className="">{data.username}</p>
                 <div className="sm:col-span-2"><SectionDivider/></div>
-                <label className="pl-5 pr-10 font-semibold">Adresse e-mail</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <AlternateEmailIcon className="w-5 h-5"/>
+                    <label>Adresse e-mail</label>
+                </div>
                 <p className="">{data.email}</p>
                 <div className="sm:col-span-2"><SectionDivider/></div>
-                <label className="pl-5 pr-10 font-semibold">Numéro de téléphone</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <PhoneInTalkIcon className="w-5 h-5"/>
+                    <label>Numéro de téléphone</label>
+                </div>
                 <p className="">{data.phoneNumber}</p>
                 <div className="sm:col-span-2"><SectionDivider/></div>
-                <label className="pl-5 pr-10 font-semibold">Rôle</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <AdminPanelSettingsIcon className="w-5 h-5"/>
+                    <label>Rôle</label>
+                </div>
                 <p className="">{data.role}</p>
                 <div className="sm:col-span-2"><SectionDivider/></div>
-                <label className="pl-5 pr-10 font-semibold">Membre de l'équipe</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <SupervisorAccountIcon className="w-5 h-5"/>
+                    <label>Membre de l'équipe</label>
+                </div>
                 <p className="">{data.teams ? data.teams.name : 'Aucune'}</p>
                 <div className="sm:col-span-2"><SectionDivider/></div>
-                <label className="pl-5 pr-10 font-semibold">Compte créé le</label>
+                <div className="pl-5 pr-10 font-semibold flex items-center gap-2">
+                    <CalendarTodayIcon className="w-5 h-5"/>
+                    <label>Compte créé le</label>
+                </div>
                 <p className="">{data.createdAt ? new Date(data.createdAt).toLocaleDateString() : 'N/A'}</p>
             </div>
             <SectionDivider/>
