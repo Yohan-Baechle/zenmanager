256712fd57fa145adf430846567fa8cb54f91e05
Yohan-Baechle <baechle.yohan@gmail.com>
2025-11-29 21:55:47 +0100

    fix: remove unused Users and Teams pages from sidebar and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,7 @@ import { AuthProvider } from './context/AuthProvider'
 import Layout from './components/layout/Layout'
 import ProtectedRoute from './components/layout/ProtectedRoute'
 import LoginPage from './pages/auth/LoginPage'
-import UsersPage from './pages/users/UsersPage'
-import CreateUserPage from './pages/users/CreateUserPage'
-import EditUserPage from './pages/users/EditUserPage'
 import ProfilePage from './pages/users/ProfilePage'
-import TeamsPage from './pages/teams/TeamsPage'
-import CreateTeamPage from './pages/teams/CreateTeamPage'
-import EditTeamPage from './pages/teams/EditTeamPage'
 import ClockPage from './pages/clocks/ClockPage'
 import EmployeeDashboard from './pages/dashboard/EmployeeDashboard'
 import ManagerDashboard from './pages/dashboard/ManagerDashboard'
@@ -54,45 +48,9 @@ function App() {
                             </ProtectedRoute>
                         } />
 
-                        <Route path="/users" element={
-                            <ProtectedRoute requiredRole={['manager']}>
-                                <UsersPage />
-                            </ProtectedRoute>
-                        } />
-
-                        <Route path="/users/create" element={
-                            <ProtectedRoute requiredRole={['manager']}>
-                                <CreateUserPage />
-                            </ProtectedRoute>
-                        } />
-
-                        <Route path="/users/edit/:id" element={
-                            <ProtectedRoute requiredRole={['manager']}>
-                                <EditUserPage />
-                            </ProtectedRoute>
-                        } />
-
                         <Route path="/profile" element={
                             <ProtectedRoute>
                                 <ProfilePage />
-                            </ProtectedRoute>
-                        } />
-
-                        <Route path="/teams" element={
-                            <ProtectedRoute requiredRole={['manager']}>
-                                <TeamsPage />
-                            </ProtectedRoute>
-                        } />
-
-                        <Route path="/teams/create" element={
-                            <ProtectedRoute requiredRole={['manager']}>
-                                <CreateTeamPage />
-                            </ProtectedRoute>
-                        } />
-
-                        <Route path="/teams/edit/:id" element={
-                            <ProtectedRoute requiredRole={['manager']}>
-                                <EditTeamPage />
                             </ProtectedRoute>
                         } />
 
